c572a62a61976f97b37a6e41f04da72fcb66fce8
newmersedez <trishkk2001@gmail.com>
2022-02-20 15:01:00 +0300

    ex00

---
+++ module_02/ex00/Fixed.cpp
@@ -0,0 +1,37 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_value = 0;
+}
+
+Fixed::Fixed(const Fixed& obj)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->_value = obj.getRawBits();
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed& obj)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_value = obj.getRawBits();
+	return (*this);
+}
+
+int     Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
+}
+
+void    Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -1,17 +1,19 @@
 #pragma once
 
+#include <iostream>
+
 class Fixed
 {
 private:
     int              _value;
-    static const int _fractional_bits;
+    static const int _fractional_bits = 8;
 
 public:
     Fixed();
     Fixed(const Fixed& obj);
     ~Fixed();
 
-    Fixed   operator=(const Fixed& obj);
+    Fixed&	operator=(const Fixed& obj);
     int     getRawBits( void ) const;
     void    setRawBits( int const raw );
 };
